982d7add701e6d336f45c4afb12e4cd796634703
sarahcamargosantos-ship-it <sarah.camargo.santos@escola.pr.gov.br>
2026-09-21 09:45:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const botoes= document.querySelectorAII("button");
 
+    botoes.forEach( function(botao) {
+      let curtiu = false;
+      botao.addEventlistener("click", botaoClicado),
+  function botaoClicado() {
+  console.log("fui clicado");
+  let texto= botao.querySelector("span");
+     if (curtiu === false) { 
+     textao.textContent++;
+  }
+  }
+  });
